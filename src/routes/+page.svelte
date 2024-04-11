78e46909e0b2c3cccc9d771af309bdba372d5640
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-11 21:05:39 +0200

    🍕 Add OG

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,6 +25,15 @@
 	<meta name="author" content="echo" />
 	<meta name="robots" content="index, follow" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<meta property="og:title" content="Pizza-formelen" />
+	<meta
+		property="og:description"
+		content="Hvor mange pizzaer trenger du til antall personer? Finn ut med pizza-formelen."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="/opengraph.png" />
+	<meta property="og:url" content="https://formel.pizza" />
 </svelte:head>
 
 <PizzaFall />
