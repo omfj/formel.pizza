dc1fc852d01be84b0d73f8a000418b0f7d4132d9
Ole Magnus Fon Johnsen <me@omfj.no>
2024-05-14 12:52:29 +0200

    🍕 Update pizza

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import PizzaFall from '$lib/PizzaFall.svelte';
 	import { calculatePizzas, pizza } from '$lib/pizza';
 
-	let people = $state();
-	let message = $state<string | number>('Fyll inn antall personer');
+	const COUNT_PARAM = 'count';
+
+	let countParam = Number($page.url.searchParams.get(COUNT_PARAM));
+	let count = $state<number>(!isNaN(countParam) ? countParam : 0);
 
 	$effect(() => {
-		if (people && !isNaN(Number(people))) {
-			message = calculatePizzas(Number(people));
-		} else {
-			message = 'Fyll inn antall personer';
-		}
+		const params = new URLSearchParams($page.url.search);
+
+		/**
+		 * Update the count param in the URL when the count changes
+		 *
+		 * Remove the param if the count is 0
+		 */
+		if (count) params.set(COUNT_PARAM, count.toString());
+		else params.delete(COUNT_PARAM);
+
+		goto(`?${params.toString()}`, { replaceState: true, noScroll: true, keepFocus: true });
 	});
 
 	$effect(() => {
+		/**
+		 * Console log the pizza ascii art
+		 */
 		console.log(pizza);
 	});
 </script>
@@ -41,34 +54,45 @@
 
 <PizzaFall />
 
-<main class="py-24 max-w-[648px] w-full z-10 mx-auto mx-auto space-y-12 px-4">
-	<h1 class="text-4xl">Pizza-formelen</h1>
+<main class="py-24 max-w-[648px] w-full z-30 mx-auto mx-auto space-y-12 px-4">
+	<h1 class="text-5xl font-medium font-serif">Pizza-formelen</h1>
 
 	<section class="space-y-4">
-		<h2 class="text-2xl font-medium mb-4">Formelen</h2>
+		<h2 class="text-3xl font-medium mb-4 font-serif">Formelen</h2>
 
-		<p>⌈3 / 8 × antall personer⌉ = antall pizza</p>
+		<p class="text-lg">⌈3 / 8 × antall personer⌉ = antall pizza</p>
 	</section>
 
 	<section class="space-y-4">
-		<h2 class="text-2xl font-medium mb-4">Prøv selv</h2>
+		<h2 class="text-3xl font-medium mb-4 font-serif">Kalkulator</h2>
 
 		<input
 			type="number"
-			class="border border-gray-300 p-2 w-full"
-			bind:value={people}
+			class="border border-gray-300 bg-white px-4 py-2 w-full rounded-lg"
+			bind:value={count}
 			placeholder="Antall personer"
 		/>
 
-		<p class="text-lg">
-			{#if !isNaN(Number(message))}
-				{#each Array.from({ length: Number(message) }) as _, i}
-					<span>🍕</span>
-				{/each}
-				<span>=</span> <span>{message}</span>
+		<div class="text-2xl">
+			{#if count && count > 0}
+				{@const pizzas = calculatePizzas(count)}
+				<p>
+					{#each Array.from({ length: pizzas }) as _, i}
+						<span>🍕</span>
+					{/each}
+					<span>=</span> <span>{pizzas}</span>
+				</p>
 			{:else}
-				<span>{message}</span>
+				<p class="text-gray-500 text-xl">Fyll inn antall personer</p>
 			{/if}
-		</p>
+		</div>
 	</section>
 </main>
+
+<footer>
+	<div class="py-4 text-center">
+		<p class="text-sm text-gray-500 hover:text-gray-700 transition-colors">
+			<a href="https://echo.uib.no" class="underline">echo.uib.no</a>
+		</p>
+	</div>
+</footer>
